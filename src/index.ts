c82e711b7eeb1739ece230ddb44746ce658b5af9
Jan Bevers <jan.bevers@be.ey.com>
2020-03-05 11:51:36 +0100

    Task 2: Add a User type to the API

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { queryType, makeSchema, stringArg } from 'nexus'
+import { queryType, makeSchema, stringArg, objectType } from 'nexus'
 import { ApolloServer } from 'apollo-server'
 
 const Query = queryType({
@@ -9,12 +9,28 @@ const Query = queryType({
         name: stringArg()
       },
       resolve: (_, args) => `Hello ${args.name || 'Nexus'}`
+    }),
+    t.field('User', {
+      type: 'User'
+    })
+  }
+})
+
+const User = objectType({
+  name: 'User',
+  definition(t) {
+    t.id('id'),
+    t.string('email', {
+      nullable: true
+    }),
+    t.string('name', {
+      nullable: true
     })
   }
 })
 
 const schema = makeSchema({
-  types: [Query],
+  types: [Query, User],
   outputs: {
     schema: __dirname + '/../schema.graphql',
     typegen: __dirname + '/generated/types.ts'
